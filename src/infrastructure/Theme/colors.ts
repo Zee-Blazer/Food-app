e1f71b0caf6c1c3dfaab0ce4756652d361fe38ae
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-01-17 20:27:55 +0100

    started working on the project

---
+++ src/infrastructure/Theme/colors.ts
@@ -0,0 +1,42 @@
+
+interface Ele {
+    primary: String,
+    secondary: String,
+    tertiary: String
+}
+
+interface Colors {
+    bg: Ele,
+    borders: Ele,
+    icons: Ele,
+    text: Ele,
+    buttons: Ele
+}
+
+export const colors: Colors = {
+    bg: {
+        primary: "",
+        secondary: "",
+        tertiary: ""
+    },
+    borders: {
+        primary: "",
+        secondary: "",
+        tertiary: ""
+    },
+    icons: {
+        primary: "",
+        secondary: "",
+        tertiary: ""
+    },
+    text: {
+        primary: "",
+        secondary: "",
+        tertiary: ""
+    },
+    buttons: {
+        primary: "",
+        secondary: "",
+        tertiary: ""
+    }
+};
